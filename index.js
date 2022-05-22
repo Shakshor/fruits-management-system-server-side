cd50e042d6b3b98d5c27a670a1f68e0109da7188
Shakshor <shakshor584@gmail.com>
2022-05-22 16:35:33 +0600

    adding post api for my-items page

--- index.js
+++ index.js
@@ -15,21 +15,28 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdydn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
+
 async function run() {
     try {
         await client.connect();
         console.log('db connected');
         const itemCollection = client.db('fruits-management').collection('items');
+        const myItemCollection = client.db('fruits-management').collection('myItems');
 
         // load the items api
         app.get('/item', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+        // get my items
+        app.get('/myItem', async (req, res) => {
+            const email = req.query.email;
+            //console.log(email);
+            const query = { email: email };
+            const cursor = myItemCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         })
@@ -41,6 +48,17 @@ async function run() {
             const item = await itemCollection.findOne(query);
             res.send(item);
         });
+
+
+
+
+        // POST (my item)
+        app.post('/myItem', async (req, res) => {
+            const newItem = req.body;
+            const result = await myItemCollection.insertOne(newItem);
+            res.send(result);
+        });
+
 
         // POST
         app.post('/item', async (req, res) => {
@@ -70,6 +88,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // DELETE
+        app.delete('/myItem/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await myItemCollection.deleteOne(query);
             res.send(result);
         })
 
